08e8a7acbbac99494b82098525c1bf9340e35185
Nazarov Yuriy <nazarov.yuriy.pavlovich@gmail.com>
2012-12-17 04:11:54 +0400

    somethink

--- graph.js
+++ graph.js
@@ -7,19 +7,95 @@
  */
 document.addEventListener("DOMContentLoaded", load, false);
 var a = 1;
-var board;
+var process_board;
+var dist_func_board;
+var dens_func_board;
+var histogram_board;
 
-function g(x){
-    return a * Math.sin(x);
+var x = [];
+var y = [];
+
+function generate_process(){
+    for(var i = 0 ; i<300;i++){
+        x[i] = i;
+        y[i] = Math.random();
+    }
+}
+
+function dist_func(x){
+    var sigma = document.forms['params_form'].param_sigm.value;
+    var mu = document.forms['params_form'].param_mu.value;
+    var ret = Math.exp( -(x-mu)*(x-mu)/(2*sigma*sigma) )/ (sigma * Math.sqrt(2 * Math.PI));
+    return ret;
+}
+
+function dens_func(x){
+    return a * Math.cos(x)*Math.cos(x);
+}
+
+function histogram(x){
+    return a * Math.sin(x)*Math.sin(x);
 }
 
 function load(){
-    board = JXG.JSXGraph.initBoard('jxgbox', {boundingbox:[-4,4,4,-4], keepaspectratio: true, axis:true});
-    board.create('functiongraph', [g,-4,4]);
+    generate_process();
+
+    process_board = JXG.JSXGraph.initBoard('process_div', {boundingbox:[0,1,300,-0.1], axis:true});
+    process_board.create('curve', [x,y]);
+
+    dist_func_board = JXG.JSXGraph.initBoard('dist_func_div', {boundingbox:[-5,1,5,-0.1], axis:true});
+    dist_func_board.create('functiongraph', [dist_func,-4,4]);
+
+    dens_func_board = JXG.JSXGraph.initBoard('dens_func_div', {boundingbox:[-4,4,4,-4], keepaspectratio: true, axis:true});
+    dens_func_board.create('functiongraph', [dens_func,-4,4]);
+
+    histogram_board = JXG.JSXGraph.initBoard('histogram_div', {boundingbox:[-4,4,4,-4], keepaspectratio: true, axis:true});
+    histogram_board.create('functiongraph', [histogram,-4,4]);
+
+    process_div   = document.getElementById("process_div");
+    dist_func_div = document.getElementById("dist_func_div");
+    dens_func_div = document.getElementById("dens_func_div");
+    histogram_div = document.getElementById("histogram_div");
 }
 
 function replot(){
-   a = Math.random();
-    board.update();
+    generate_process();
+    process_board.update();
+    dist_func_board.update();
+    dens_func_board.update();
+    histogram_board.update();
     return false;
+}
+
+var process_div;
+var dist_func_div;
+var dens_func_div;
+var histogram_div;
+
+function show_process(){
+    process_div.style.display = "block";
+    dist_func_div.style.display = "none";
+    dens_func_div.style.display = "none";
+    histogram_div.style.display = "none";
+}
+
+function show_dist_func(){
+    process_div.style.display = "none";
+    dist_func_div.style.display = "block";
+    dens_func_div.style.display = "none";
+    histogram_div.style.display = "none";
+}
+
+function show_dens_func(){
+    process_div.style.display = "none";
+    dist_func_div.style.display = "none";
+    dens_func_div.style.display = "block";
+    histogram_div.style.display = "none";
+}
+
+function show_histogram(){
+    process_div.style.display = "none";
+    dist_func_div.style.display = "none";
+    dens_func_div.style.display = "none";
+    histogram_div.style.display = "block";
 }
